b45b11187e0373ca2098ce0505baaa581c500d70
Thisara <sasmithajayasinghe1@gmail.com>
2024-04-05 14:59:21 +0530

    created fetch by category

--- src/main/java/com/quizzapp/quizzapp/dao/QuestionDao.java
+++ src/main/java/com/quizzapp/quizzapp/dao/QuestionDao.java
@@ -4,8 +4,12 @@ import com.quizzapp.quizzapp.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {  //all database stuff handled by JpaRepository
                                                                          //Question - database table name, Integer - databse primary key title
 
+
+    List<Question> findByCategory(String category);  //findByCategory <- Category sql will find out
 }
